a08830025c692e9c40fc841e6b4ceade0713b437
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-06-20 14:54:40 +0200

    Refactoring

--- ORION.Map/obj/Debug/net6.0/ORION.Map.AssemblyInfo.cs
+++ ORION.Map/obj/Debug/net6.0/ORION.Map.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORION.Map")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214f07377297a7db4036894394dac07eb224294f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORION.Map")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORION.Map")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
