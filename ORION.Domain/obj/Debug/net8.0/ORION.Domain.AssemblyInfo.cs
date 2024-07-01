8e574d1d0f6415e645871d133e9b959cd0eb3679
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-07-01 12:28:23 +0200

    Still refactoring

--- ORION.Domain/obj/Debug/net8.0/ORION.Domain.AssemblyInfo.cs
+++ ORION.Domain/obj/Debug/net8.0/ORION.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55422d3f615f79189429ac9573b475890fdf5ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7917eca5becbf071bea4e7c6e479fc620d982e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
