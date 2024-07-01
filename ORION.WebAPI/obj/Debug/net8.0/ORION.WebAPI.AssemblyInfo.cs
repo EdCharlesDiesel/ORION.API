6653a35edda6f27c8d37c9fc666b4e501dded7f8
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-07-01 08:27:07 +0200

    Refactoring

--- ORION.WebAPI/obj/Debug/net8.0/ORION.WebAPI.AssemblyInfo.cs
+++ ORION.WebAPI/obj/Debug/net8.0/ORION.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORION.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52eca2ca205f25d3cb11545cb8ed276d1218beaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365488fd588a9e900b3bf19d6d825c12529e2486")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORION.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORION.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
