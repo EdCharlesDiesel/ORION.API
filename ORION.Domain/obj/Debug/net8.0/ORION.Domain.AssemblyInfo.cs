538226d30af42e17568e80dfa7beb9e2d858f4d1
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-08-23 14:58:48 +0200

    .
    .

--- ORION.Domain/obj/Debug/net8.0/ORION.Domain.AssemblyInfo.cs
+++ ORION.Domain/obj/Debug/net8.0/ORION.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55422d3f615f79189429ac9573b475890fdf5ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e3136c2ee6341b7334e0e26064bfb4af38f3e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORION.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
