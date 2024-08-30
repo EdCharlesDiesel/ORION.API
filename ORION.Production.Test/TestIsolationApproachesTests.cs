e2638cfec1352fa5b43cadde303cffdcb8cf01d2
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-08-30 19:20:44 +0200

    Added relavant classes

---
+++ ORION.Production.Test/TestIsolationApproachesTests.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ORION.HumanResources.Business;
+using ORION.HumanResources.DataAccess.DbContexts;
+using ORION.HumanResources.DataAccess.Entities;
+using ORION.HumanResources.DataAccess.Services;
+using ORION.HumanResources.Services.Test;
+using ORION.HumanResources.Test.HttpMessageHandlers;
+using Xunit;
+using Xunit.Sdk;
+
+namespace ORION.HumanResources.Test
+{
+    public class TestIsolationApproachesTests
+    {
+        [Fact(Skip = "Skipping this one for demo reasons.")]
+        public async Task AttendCourseAsync_CourseAttended_SuggestedBonusMustCorrectlyBeRecalculated()
+        {
+            // Arrange
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HumanResourcesDbContext>()
+                      .UseSqlite(connection);
+
+            var dbContext = new HumanResourcesDbContext(optionsBuilder.Options);
+            dbContext.Database.Migrate();
+
+            var employeeManagementDataRepository =
+               new EmployeeManagementRepository(dbContext);
+
+            var employeeService = new EmployeeService(
+                employeeManagementDataRepository,
+                new EmployeeFactory());
+
+            // get course from database - "Dealing with Customers 101"
+            var courseToAttend = await employeeManagementDataRepository
+                .GetCourseAsync(Guid.Parse("844e14ce-c055-49e9-9610-855669c9859b"));
+
+            // get existing employee - "Megan Jones"
+            var internalEmployee = await employeeManagementDataRepository
+                .GetInternalEmployeeAsync(Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            if (courseToAttend == null || internalEmployee == null)
+            {
+                throw new XunitException("Arranging the test failed");
+            }
+
+            // expected suggested bonus after attending the course
+            var expectedSuggestedBonus = internalEmployee.YearsInService
+                * (internalEmployee.AttendedCourses.Count + 1) * 100;
+
+            // Act
+            await employeeService.AttendCourseAsync(internalEmployee, courseToAttend);
+
+            // Assert
+            Assert.Equal(expectedSuggestedBonus, internalEmployee.SuggestedBonus);
+        }
+
+        [Fact]
+        public async Task PromoteInternalEmployeeAsync_IsEligible_JobLevelMustBeIncreased()
+        {
+            // Arrange
+            var httpClient = new HttpClient(
+                new TestablePromotionEligibilityHandler(true));
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+            var promotionService = new PromotionService(httpClient,
+                new EmployeeManagementTestDataRepository());
+
+            // Act
+            await promotionService.PromoteInternalEmployeeAsync(internalEmployee);
+
+            // Assert
+            Assert.Equal(2, internalEmployee.JobLevel);
+        }
+
+    }
+}
--- ORION.Purchasing.Test/obj/Debug/net8.0/ORION.Purchasing.Test.AssemblyInfo.cs
+++ ORION.Purchasing.Test/obj/Debug/net8.0/ORION.Purchasing.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORION.Purchasing.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93932a0e62187b79782605ec597882527fda77d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c87d7b36973e228fec9dfaab57f9caa3cf4d673")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORION.Purchasing.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORION.Purchasing.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
